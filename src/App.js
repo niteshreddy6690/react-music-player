4b6fcdbd2983cad51f9b3c4e998b178f47c22180
nitesh <zxxzx>
2020-07-07 20:12:07 +0530

    initial push'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Musicplayer from "./Components/Musicplayer";
 
 function App() {
+    const [active,setActive]=useState(true)
+    const  handelActive=()=>{
+        setActive(!active)
+    }
   return (
     <div className="App">
         <Musicplayer/>
